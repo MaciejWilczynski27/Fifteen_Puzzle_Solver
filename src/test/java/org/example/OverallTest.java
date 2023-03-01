7c12647f81c502421e71d1c084da59bdde89f920
242560 Wilczyński Maciej <242560@edu.p.lodz.pl>
2023-03-01 22:13:17 +0100

    Startowy pom

---
+++ src/test/java/org/example/OverallTest.java
@@ -0,0 +1,9 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+public class OverallTest {
+    @Test
+    public void basicTest() {
+        assertFalse(1==2);
+    }
+}
